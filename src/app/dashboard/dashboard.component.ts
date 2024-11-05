69f2fdf544e9b13fc627ca5ce69eace5cf6544d5
mohamedMahmoudElkady <mohamedmahmoudelkady111@gmail.com>
2024-11-05 21:14:21 +0200

    added available appointments for doctor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,24 +1,35 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth-service.service';
 import { User } from 'firebase/auth';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { UsersScreenComponent } from "../users-screen/users-screen.component";
+import { UsersScreenComponent } from '../users-screen/users-screen.component';
 import { DoctorsScreenComponent } from '../doctors-screen/doctors-screen.component';
 import { AppUser } from '../models/app-user.interface';
-import { DoctorProfileComponent } from "../doctor-profile/doctor-profile.component";
-import { AppointmentsComponent } from "../appointments/appointments.component";
-import { DoctorAppointmentsComponent } from "../doctor-appointments/doctor-appointments.component";
-import { TotalRevenueComponent } from "../total-revenue/total-revenue.component";
+import { DoctorProfileComponent } from '../doctor-profile/doctor-profile.component';
+import { AppointmentsComponent } from '../appointments/appointments.component';
+import { DoctorAppointmentsComponent } from '../doctor-appointments/doctor-appointments.component';
+import { TotalRevenueComponent } from '../total-revenue/total-revenue.component';
 import { ToastrService } from 'ngx-toastr'; // Import ToastrService
 import { Router } from '@angular/router';
+import { AvailableAppointmentsComponent } from '../available-appointments/available-appointments.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [FormsModule, CommonModule, UsersScreenComponent, DoctorsScreenComponent, DoctorProfileComponent, AppointmentsComponent, DoctorAppointmentsComponent, TotalRevenueComponent],
+  imports: [
+    FormsModule,
+    CommonModule,
+    UsersScreenComponent,
+    DoctorsScreenComponent,
+    DoctorProfileComponent,
+    AppointmentsComponent,
+    DoctorAppointmentsComponent,
+    TotalRevenueComponent,
+    AvailableAppointmentsComponent,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent implements OnInit {
   user: AppUser | null = null;
@@ -31,8 +42,12 @@ export class DashboardComponent implements OnInit {
     return this.user?.role === 'doctor';
   }
   // Method to toggle the expanded state of a branch
-  
-  constructor(private authService: AuthService,private router: Router, private toastr: ToastrService) {}
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
   ngOnInit() {
     // Subscribe to the user data
     this.authService.currentUser$.subscribe((user) => {
@@ -40,7 +55,7 @@ export class DashboardComponent implements OnInit {
       console.log('Current user:', this.user);
     });
     if (this.user) {
-      this.activeTab = (this.user.role === 'admin') ? 'Users' : 'Manage Profile';
+      this.activeTab = this.user.role === 'admin' ? 'Users' : 'Manage Profile';
     }
   }
 
@@ -53,7 +68,7 @@ export class DashboardComponent implements OnInit {
       this.authService.logout();
       // Show toast notification
       this.toastr.success('Logged out successfully!', 'Success');
-  
+
       // Redirect to the login page
       this.router.navigate(['/login']); // Adjust the path as necessary
     } catch (error) {
@@ -61,5 +76,4 @@ export class DashboardComponent implements OnInit {
       this.toastr.error('Logout failed. Please try again.', 'Error');
     }
   }
- 
 }
